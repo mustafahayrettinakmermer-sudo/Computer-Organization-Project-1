dc80990ccbbcbef85d4e505448d00d122f44dd53
Mustafa <mustafahayrettinakmermer@gmail.com>
2026-05-10 19:45:53 +0200

    start 1

---
+++ demos.h
@@ -0,0 +1,12 @@
+#ifndef DEMOS_H
+#define DEMOS_H
+
+void stack_heap_demo();
+void pointer_arithmetic_demo();
+void array_pointer_demo();
+void struct_layout_demo();
+void dynamic_memory_demo();
+void memory_error_demo();
+void function_pointer_demo();
+
+#endif
